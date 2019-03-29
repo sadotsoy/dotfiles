2dc83fd22435e7d30873940a6cc60edc908d7ebc
sadotsoy <sadot@mariachi.io>
2019-03-29 10:05:52 -0600

    updates

--- osxWM/osxWM.sh
+++ osxWM/osxWM.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 echo "clone tap CHUNKWM and SKHD"
 brew tap koekeishiya/formulae
 
@@ -7,9 +9,15 @@ brew install chunkwm
 echo "Installing Skhd"
 brew install koekeishiya/formulae/skhd
 
+echo "Moving Configs"
+mv "chunkwmrc" "~/.configs/"
+mv "skhdrc" "~/.configs/"
+mv "scripts/" "~/.configs/"
+
+
 echo "Installing configs"
-ln -s "chunkwmrc" "${HOME}/.chunkwmrc"
-ln -s "skhdrc" "${HOME}/.skhdrc"
+ln -s "${HOME}/.config/chunkwmrc" "${HOME}/.chunkwmrc"
+ln -s "${HOME}/.config/skhdrc" "${HOME}/.skhdrc"
 
 echo "Starting services CHUNKWM + SKHD"
 brew services start chunkwm
