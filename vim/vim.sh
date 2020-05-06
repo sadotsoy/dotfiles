cc8a06402b371bd767b3c4f354c37615d4b4b9b5
Sadot Cortes <sadotcortes@gmail.com>
2020-05-06 15:17:55 -0500

    vim iug dotfiles deleted

--- vim/vim.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Install plugin manager and set the vimrc
-echo "Installing Vundle Vim."
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-clear
-echo "Moving virmc to HOME"
-mv -f vimrc ~/.vimrc
-
