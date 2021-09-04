4722e644ad1d896813c518acfdbdc01a0a86fce1
sadotsoy <sadotcortes@gmail.com>
2021-09-04 09:47:20 -0500

    webpack configs
    [+] develop
    [+] build
    [+] config base

---
+++ webpack/webpack.config.development.js
@@ -0,0 +1,16 @@
+const { merge } = require('webpack-merge');
+const path = require('path');
+
+const config = require('./webpack.config');
+
+module.exports = merge(config, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    writeToDisk: true,
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+  },
+});
+
